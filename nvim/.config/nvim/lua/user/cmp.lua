5763bc82fbb684d5523f2feb5337d4cbff4acbc1
Felipe Ramos <f3liperamos@gmail.com>
2022-11-17 10:46:11 +0100

    adding cmp-nvim-lua and cmp-nvim-lsp packages, fixing typo and sorting cmp sources

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -49,14 +49,13 @@ cmp.setup({
 	formatting = {
 		format = function(entry, vim_item)
 			-- Kind icons
-			vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
 			-- Source
 			vim_item.menu = ({
 				buffer = "[Buffer]",
-				nvim_lsp = "[LSP]",
 				luasnip = "[LuaSnip]",
-				nvim_lua = "[Lua]",
-				latex_symbols = "[LaTeX]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[nvim_lua]",
 			})[entry.source.name]
 			return vim_item
 		end
@@ -82,6 +81,8 @@ cmp.setup({
 		select = false,
 	},
 	sources = {
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -56,10 +56,14 @@ return packer.startup(function(use)
 	} }}
 
 	-- completion
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
+	use {
+		"hrsh7th/nvim-cmp"
+		,"hrsh7th/cmp-buffer"
+		,"hrsh7th/cmp-path"
+		,"hrsh7th/cmp-cmdline"
+		,"hrsh7th/cmp-nvim-lsp"
+		,"hrsh7th/cmp-nvim-lua"
+	}
 
 	-- nvim-cmp requires one snippet engine
 	use({"L3MON4D3/LuaSnip", tag = "v1.*"})
