745a091d67e3ef6d4acb2e4852c3bcdb6669e0b2
Felipe Ramos <f3liperamos@gmail.com>
2022-11-18 00:12:09 +0100

    let user.notify return a compatible fallback

--- nvim/.config/nvim/lua/user/notify.lua
+++ nvim/.config/nvim/lua/user/notify.lua
@@ -1,12 +1,26 @@
--- check if nvim-notify exists otherwise return the defaul notify
+-- check if nvim-notify exists otherwise return an error message
+-- and a fallback to original vim.notify
 local status_ok, notify = pcall(require, 'notify')
 if not status_ok then
-	return vim.notify
+	local mapping = {
+		["debug"] = vim.log.levels.DEBUG,
+		["error"] = vim.log.levels.ERROR,
+		["info"] = vim.log.levels.INFO,
+		["off"] = vim.log.levels.OFF,
+		["trace"] = vim.log.levels.TRACE,
+		["warn"] = vim.log.levels.WARN,
+	}
+
+	vim.notify("nvim-notify not found, check installation", mapping.error)
+
+	return function(message, log_level, _)
+		vim.notify(message, mapping[log_level] or nil)
+	end
 end
 
-notify.setup({
-	-- catppuccin base color
-	background_colour = "#1e1e2e"
-})
+-- catppuccin base color
+notify.setup({ background_colour = "#1e1e2e" })
+
+vim.notify = notify
 
 return notify
