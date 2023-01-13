15a3346614ee4ab3427e995427384698e7de2954
Felipe Ramos <f3liperamos@gmail.com>
2023-01-13 20:21:32 +0100

    settings catppuccin integrations explicitly

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,14 +1,35 @@
 return {
-	"catppuccin/nvim",
-	version = "v0.2",
-	name = "catppuccin",
-	config = function()
-		local notify = require("util.notify")
+	{
+		"catppuccin/nvim",
+		version = "v0.2",
+		name = "catppuccin",
+		config = function()
+			local notify = require("util.notify")
+			local status_ok, catppuccin = pcall(require, "catppuccin")
+			if not status_ok then
+				notify("catppuccin not found", "error", { title = "failed to load colorscheme" })
+				return
+			end
 
-		local status_ok, _ = pcall(vim.cmd.colorscheme, "catppuccin")
-		if not status_ok then
-			notify("catppuccin not found", "error", { title = "failed to load colorscheme" })
-			return
-		end
-	end,
+			catppuccin.setup({
+				integrations = {
+					cmp = true,
+					dashboard = true,
+					gitsigns = true,
+					mason = true,
+					notify = true,
+					nvimtree = true,
+					telescope = true,
+					treesitter = true,
+					which_key = true,
+
+					-- special integrations, check their .lua
+					bufferline = true,
+					fidget = true,
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
