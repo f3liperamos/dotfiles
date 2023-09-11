87dab8489fab730c05aa0da57432a038a70adaf4
Felipe Ramos <f3liperamos@gmail.com>
2023-09-12 01:54:08 +0200

    feat(mini) Adding mini.{surround,pairs,indentscope}

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"echasnovski/mini.pairs",
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		config = function()
+			require("mini.indentscope").setup()
+		end,
+	},
+}
