99bd9f0625165af15536ade1f053754dc5c95266
Felipe Ramos <f3liperamos@gmail.com>
2023-10-25 13:56:59 +0200

    chore(mini): Removing unnecessary config property, changing mini.surround mappings

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,20 +1,27 @@
 return {
 	{
 		"echasnovski/mini.pairs",
-		config = function()
-			require("mini.pairs").setup()
-		end,
+		enabled = false,
 	},
 	{
 		"echasnovski/mini.surround",
-		config = function()
-			require("mini.surround").setup()
-		end,
+		opts = {
+			mappings = {
+				add = "gza", -- Add surrounding in Normal and Visual modes
+				delete = "gzd", -- Delete surrounding
+				find = "gzf", -- Find surrounding (to the right)
+				find_left = "<Leader>gzF", -- Find surrounding (to the left)
+				highlight = "gzh", -- Highlight surrounding
+				replace = "gzr", -- Replace surrounding
+				update_n_lines = "gzn", -- Update `n_lines`
+			},
+		},
 	},
 	{
 		"echasnovski/mini.indentscope",
-		config = function()
-			require("mini.indentscope").setup()
-		end,
+		version = false, -- wait till new 0.7.0 release to put it back on semver
+		opts = {
+			draw = { delay = 200 },
+		},
 	},
 }
