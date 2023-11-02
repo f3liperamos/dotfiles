0358cb930e4d05904683fabfcefde2b6d18b3bdd
Felipe Ramos <f3liperamos@gmail.com>
2023-11-02 10:17:45 +0100

    chore(cmp-cmdline): Allow picking items with jk

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -113,7 +113,10 @@ return {
 		})
 
 		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
+			mapping = cmp.mapping.preset.cmdline({
+				["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+			}),
 			sources = {
 				{ name = "cmdline" },
 				{ name = "path" },
