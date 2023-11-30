e529866bfe658cf9f60ed58ba56a8178ebb4112a
Felipe Ramos <f3liperamos@gmail.com>
2023-11-30 23:00:03 +0100

    chore(lazy.nvim) setting opts.ui.border=single

--- nvim/.config/nvim/lua/config/plugins.lua
+++ nvim/.config/nvim/lua/config/plugins.lua
@@ -12,8 +12,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
-	ui = {
-		border = { "/", "-", "\\", "|" },
-	},
-})
+require("lazy").setup("plugins", { ui = { border = "single" } })
