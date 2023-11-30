f2b51ea25865d9d1e9c3a83dabe62faa73f99cc7
Felipe Ramos <f3liperamos@gmail.com>
2023-11-30 23:54:29 +0100

    chore(keymaps) adding description to some keymaps in keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,14 +1,20 @@
 local keymap_set = vim.keymap.set
 local opts = { silent = true, noremap = true }
 
+---@param rhs table
+local extend_opts = function(rhs)
+	rhs = rhs or {}
+	return vim.tbl_deep_extend("force", opts, rhs)
+end
+
+keymap_set("v", "<C-c>", '"*y', extend_opts({ desc = 'Copy selection to "* register' }))
+keymap_set("n", "<C-h>", "<C-w>h", extend_opts({ desc = "Go to the left window" }))
+keymap_set("n", "<C-j>", "<C-w>j", extend_opts({ desc = "Go to the down window" }))
+keymap_set("n", "<C-k>", "<C-w>k", extend_opts({ desc = "Go to the up window" }))
+keymap_set("n", "<C-l>", "<C-w>l", extend_opts({ desc = "Go to the right window" }))
+
 -- set space to also trigger leader key
 keymap_set("n", "<Space>", "<Leader>", { remap = true })
-
--- better (?) window navigation
-keymap_set("n", "<C-h>", "<C-w>h", opts)
-keymap_set("n", "<C-j>", "<C-w>j", opts)
-keymap_set("n", "<C-k>", "<C-w>k", opts)
-keymap_set("n", "<C-l>", "<C-w>l", opts)
 
 -- insert mode quick exit
 keymap_set("i", "jk", "<Esc>", opts)
@@ -26,6 +32,3 @@ keymap_set("n", "<C-u>", "<C-u>zz", opts)
 keymap_set("n", "<C-d>", "<C-d>zz", opts)
 keymap_set("n", "n", "nzz", opts)
 keymap_set("n", "N", "Nzz", opts)
-
--- Mapping "*y to CTRL_C
-keymap_set("v", "<C-c>", '"*y', opts)
