9b5fd0a16193939968a5b8c8ae99c5e89f8c072a
Felipe Ramos <f3liperamos@gmail.com>
2023-12-01 03:59:38 +0100

    chore(mini) removed mini.indentscope, visual bugs when paired with wrap option

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -17,11 +17,4 @@ return {
 			},
 		},
 	},
-	{
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		opts = {
-			draw = { delay = 200 },
-		},
-	},
 }
