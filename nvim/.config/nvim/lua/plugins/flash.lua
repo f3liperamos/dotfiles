05af1b8ee96968cc3dca167ef81315cbbf7433d7
Felipe Ramos <f3liperamos@gmail.com>
2023-12-01 03:27:22 +0100

    feat(flash) added flash.nvim

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,47 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {
+		modes = {
+			search = {
+				search = {
+					trigger = "/",
+				},
+			},
+		},
+	},
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Flash Remote",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Flash Treesitter Search",
+		},
+	},
+}
