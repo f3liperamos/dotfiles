58ed2d484a5831e875a3bc4724e9fb7a2f141499
Felipe Ramos <f3liperamos@gmail.com>
2023-12-01 03:28:59 +0100

    fix(null-ls) removing null_ls/none_ls

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++
@@ -1,9 +0,0 @@
-local builtins = require("null-ls").builtins
-require("null-ls").setup({
-	sources = {
-		builtins.code_actions.gitsigns,
-		builtins.formatting.stylua,
-		builtins.formatting.eslint_d,
-		builtins.formatting.prettierd,
-	},
-})
