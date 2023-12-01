a18a91ec26e370dd3ed664bba01b9189d2ffac8f
Felipe Ramos <f3liperamos@gmail.com>
2023-12-01 03:56:18 +0100

    chore(gitsigns) adding VeryLazy event

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
 	"lewis6991/gitsigns.nvim",
 	dependencies = { "tpope/vim-fugitive" },
 	version = "v0.6",
+	event = "VeryLazy",
 	config = function()
 		require("gitsigns").setup({
 			current_line_blame = true,
