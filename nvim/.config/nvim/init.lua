c24b9309db6a1a4642960c1e95547de9ed0247ca
Felipe Ramos <f3liperamos@gmail.com>
2024-01-04 17:37:27 +0100

    chore(lazy): moving lazy bootstrap into init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,10 +1,26 @@
+-- Load everything unrelated to plugins
 require("config.options")
 require("config.keymaps")
-require("config.plugins")
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins
+require("lazy").setup("plugins", { ui = { border = "single" } })
 
 --[[
 -- Pending
-1. Ltex language server https://github.com/valentjn/vscode-ltex. Mason can manage it
 2. Change keybindings to `keys` property on lazy.nvim?
 3. Remove unnecessary protected calls from lazy configs. Probably not necessary anymore
 4. Cleanup migration from packer, re-check if there is any leftover
