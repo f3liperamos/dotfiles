4a2493f63acf4bf4fba93c9c35977507d393c87e
Felipe Ramos <f3liperamos@gmail.com>
2024-01-04 21:42:43 +0100

    chore(gitsigns): updating to 0.7

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,12 +1,10 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	dependencies = { "tpope/vim-fugitive" },
-	version = "v0.6",
+	version = "v0.7",
 	event = "VeryLazy",
-	config = function()
-		require("gitsigns").setup({
-			current_line_blame = true,
-			trouble = true,
-		})
-	end,
+	opts = {
+		current_line_blame = true,
+		trouble = true,
+	},
 }
