55305444b6613fcc385713be6e983fccca4c2a39
Felipe Ramos <f3liperamos@gmail.com>
2024-01-04 20:55:10 +0100

    chore(bufferline): adding plugin suffix to keymaps

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -3,17 +3,17 @@ return {
 	event = "VeryLazy",
 	version = "v4.*",
 	dependencies = {
-		"nvim-tree/nvim-web-devicons",
 		"catppuccin",
 		"nvim-telescope/telescope.nvim",
+		"nvim-tree/nvim-web-devicons",
 	},
 	keys = {
-		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Go to previous buffer" },
-		{ "<Tab>", "<Cmd>b#<CR>", desc = "Go to alternate buffer" },
-		{ "<Leader>b", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer" },
-		{ "<Leader>bb", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer" },
-		{ "<Leader>bl", ":lua require('telescope.builtin').buffers()<CR>", desc = "List buffers" },
-		{ "<Leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Close other buffers" },
+		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Go to previous buffer [Bufferline]" },
+		{ "<Tab>", "<Cmd>b#<CR>", desc = "Go to alternate buffer [Bufferline]" },
+		{ "<Leader>b", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer [Bufferline]" },
+		{ "<Leader>bb", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer [Bufferline]" },
+		{ "<Leader>bl", ":lua require('telescope.builtin').buffers()<CR>", desc = "List buffers [Bufferline]" },
+		{ "<Leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Close other buffers [Bufferline]" },
 	},
 	config = function()
 		require("bufferline").setup({
