fe4e64199e17a48de679aa95fe395913806477a6
Felipe Ramos <f3liperamos@gmail.com>
2024-03-05 20:31:09 +0100

    chore(moving-lsp-configs) Removing lsp settings files, adding it to lspconfig.lua instead

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -20,15 +20,15 @@ return {
 				map("gI", require("telescope.builtin").lsp_implementations, "[g]oto [I]mplementation")
 				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[d]ocument [s]ymbols")
-				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[w]orkspace [s]ymbols")
+				map("<leader>rn", vim.lsp.buf.rename, "[r]e[n]ame")
+				map("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
 				map("K", vim.lsp.buf.hover, "Hover Documentation")
-				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-				map("[d", vim.diagnostic.goto_prev, "Go to previous [D]iagnostic message")
-				map("]d", vim.diagnostic.goto_next, "Go to next [D]iagnostic message")
-				map("<leader>e", vim.diagnostic.open_float, "Show diagnostic [E]rror messages")
-				map("<leader>q", vim.diagnostic.setloclist, "Open diagnostic [Q]uickfix list")
+				map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
+				map("[d", vim.diagnostic.goto_prev, "Go to previous diagnostic message")
+				map("]d", vim.diagnostic.goto_next, "Go to next diagnostic message")
+				map("<leader>e", vim.diagnostic.open_float, "Show diagnostic [e]rror messages")
+				map("<leader>q", vim.diagnostic.setloclist, "Open diagnostic [q]uickfix list")
 
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
 				if client and client.server_capabilities.documentHighlightProvider then
@@ -57,33 +57,42 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-		require("mason").setup()
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				"eslint_d",
-				"prettier",
-				"prettierd",
-				"stylua",
-			},
-		})
+		local servers = {
+			-- Linters / Formatters
+			eslint_d = {},
+			prettier = {},
+			prettierd = {},
+			stylua = {},
 
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"cssls",
-				"html",
-				"jsonls",
-				"lua_ls",
-				"rust_analyzer",
-				"tsserver",
+			-- LSPs
+			cssls = {},
+			html = {},
+			jsonls = {},
+			lua_ls = {
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = { enable = false },
+					},
+				},
 			},
+			rust_analyzer = {},
+			tsserver = {},
+		}
+
+		require("mason").setup()
+		require("mason-tool-installer").setup({ ensure_installed = servers })
+		require("mason-lspconfig").setup({
 			handlers = {
 				function(server_name)
-					local server = {}
-					local settings_ok, server_settings = pcall(require, "lsp.settings." .. server_name)
-					if settings_ok then
-						server = server_settings
-					end
-
+					local server = servers[server_name] or {}
 					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 					require("lspconfig")[server_name].setup(server)
 				end,
